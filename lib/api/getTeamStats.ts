6e496d2ec9df55846cf15a3919aba801d4c53204
Sicilica <schixxism@gmail.com>
2017-11-30 11:38:36 -0700

    getTeamStats() can return null

--- lib/api/getTeamStats.ts
+++ lib/api/getTeamStats.ts
@@ -2,7 +2,7 @@ import * as t from "../types"
 
 
 
-export function getTeamStats(team: string): t.TeamStats {
+export function getTeamStats(team: string): t.TeamStats | null {
   // TODO
   return {
     wins: 1,
